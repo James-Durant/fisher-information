df75a097b1642172a2415e289bff5d3afb315807
James-Durant <james.durant@warwick.ac.uk>
2020-11-30 13:38:22 +0000

    Update experiment.py

--- utils/experiment.py
+++ utils/experiment.py
@@ -4,33 +4,35 @@ sys.path.append("../utils") # Adds higher directory to python modules path.
 import numpy as np
 import matplotlib.pyplot as plt
 
-from refnx.dataset  import ReflectDataset
-from refnx.reflect  import ReflectModel
-from refnx.analysis import Objective
+from refnx.reflect import ReflectModel
 
-from generate   import plot_objective, SCALE, DQ, BKG, BKG_RATE
-from structures import easy_sample_1, many_param_sample
+from generate   import SCALE, DQ, BKG, BKG_RATE
+from structures import easy_sample_1, many_param_sample, multiple_contrast_sample
 from structures import thin_layer_sample_1, thin_layer_sample_2
 from structures import similar_sld_sample_1, similar_sld_sample_2
 
-POINTS = 259
+POINTS = 55
 
 def simulate(structure, angle, ax, time=1, file="../utils/directbeam_wavelength.dat"):
-    theta = angle*np.pi/180 #Angle in radians
     model = ReflectModel(structure, scale=SCALE, dq=DQ, bkg=BKG)
-    q, r, r_errors = [], [], []
     
     direct_beam  = np.loadtxt(file, delimiter=',')
     wavelengths  = direct_beam[:,0]
     flux_density = direct_beam[:,1]*time
     
-    flux_density, wavelengths = np.histogram(wavelengths, POINTS, weights=flux_density)
+    theta = angle*np.pi/180 #Angle in radians
+    q = 4*np.pi*np.sin(theta) / wavelengths
+    log_q = np.logspace(np.log10(np.min(q)), np.log10(np.max(q)), POINTS+1)
+    flux_binned, q_bin_edges = np.histogram(q, log_q, weights=flux_density)
     
+    #Get the bin centres
+    q_binned = [(q_bin_edges[i] + q_bin_edges[i+1]) / 2 for i in range(POINTS)]
+
+    r, r_errors = [], []
     for i in range(POINTS):
-        wavelength = wavelengths[i]
-        flux       = flux_density[i]
+        q_point = q_binned[i]
+        flux    = flux_binned[i]
         
-        q_point = 4*np.pi*np.sin(theta) / wavelength
         r_point = model(q_point) + max(np.random.normal(1, 0.5)*BKG_RATE, 0)
         
         count       = r_point*flux
@@ -40,32 +42,24 @@ def simulate(structure, angle, ax, time=1, file="../utils/directbeam_wavelength.
         r_noisy = noisy_count / flux
         r_error = error_bar / flux
         
-        q.append(q_point)
         r.append(r_noisy)
         r_errors.append(r_error)
     
-    data = ReflectDataset([q,r,r_errors])
-    objective = Objective(model, data)
-
-    y, y_err, model = objective._data_transform(model=objective.generative())
-    # Add the data in a transformed fashion.
-    ax.errorbar(objective.data.x, y, y_err, marker="o", ms=3, lw=0, elinewidth=1, capsize=1.5)
-    #Add the prediction/fit
-    #ax.plot(objective.data.x, model, color="red", zorder=20)
-
+    ax.errorbar(q_binned, r, r_errors, marker="o", ms=3, lw=0, elinewidth=1, capsize=1.5)
     plt.xlabel("$\mathregular{Q\ (Å^{-1})}$", fontsize=11, weight='bold')
     plt.ylabel('Reflectivity (arb.)',         fontsize=11, weight='bold')
     plt.yscale('log')
     
-    return q, r, r_errors, flux_density
+    return q_binned, r, r_errors, flux_binned
     
     
 if __name__ == "__main__":
-    structure = easy_sample_1()
+    structures = multiple_contrast_sample()
     fig = plt.figure(figsize=[9,7])
     ax = fig.add_subplot(111)
-    for angle in [0.7, 2]:
-        simulate(*structure, angle, ax, time=1)
+    for structure in structures:
+        for angle in [0.7, 2]:
+            simulate(structure, angle, ax, time=10)
     plt.show()
         
     
