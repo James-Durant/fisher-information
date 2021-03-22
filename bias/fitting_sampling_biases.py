63ccb57f6099c6f3f5dc6dfe3333c76aa987fc7c
James Durant <James.Durant@warwick.ac.uk>
2021-03-22 08:16:41 +0000

    Added difference plots

--- bias/fitting_sampling_biases.py
+++ bias/fitting_sampling_biases.py
@@ -1,3 +1,4 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import os
 
@@ -5,6 +6,56 @@ from refnx.analysis import Objective, CurveFitter
 from simulate import simulate
 
 from utils import Sampler, vary_structure
+
+def difference_plots_data(structure, angle_times):
+    for i in range(5):
+        model, data = simulate(structure(), angle_times)
+
+        data_sim = data.y
+        data_true = model(data.x)
+        
+        fig1 = plt.figure(figsize=[9,7], dpi=600)
+        ax1 = fig1.add_subplot(111)
+        ax1.errorbar(data.x, data.y, data.y_err, color="black", marker="o", ms=3, lw=0, elinewidth=1, capsize=1.5)
+        ax1.plot(data.x, data_true, color="red", zorder=20)
+        ax1.set_xlabel("$\mathregular{Q\ (Å^{-1})}$", fontsize=11, weight='bold')
+        ax1.set_ylabel('Reflectivity (arb.)', fontsize=11, weight='bold')
+        ax1.set_yscale('log')
+        ax1.set_xlim(0.005, 0.25)
+        ax1.set_ylim(1e-7, 2)
+        
+        diff = (data_true - data_sim) / np.asarray([sum(x) for x in zip(data_true, data_sim)]) 
+        diff_error = 2*(data.y_err / data.y)*diff
+        
+        fig2 = plt.figure(figsize=[9,7], dpi=600)
+        ax2 = fig2.add_subplot(111)
+        ax2.errorbar(data.x, diff, diff_error, color="black", elinewidth=1, capsize=1.5)
+        ax2.set_xlabel("$\mathregular{Q\ (Å^{-1})}$", fontsize=11, weight='bold')
+        ax2.set_ylabel('Difference (arb.)', fontsize=11, weight='bold')
+        ax2.set_xlim(0.005, 0.25)
+        
+        plt.show()
+
+
+def difference_plots_models(structure, angle_times, n, q=np.arange(0.005, 0.3, 0.00005)):
+    for i in range(n):
+        objective = Objective(*simulate(structure(), angle_times))
+        
+        true = objective.model(q)
+        
+        vary_structure(objective.model.structure, random=True)
+        CurveFitter(objective).fit('differential_evolution', verbose=False)
+
+        fitted = objective.model(q)
+        
+        diff = (true - fitted) / np.asarray([sum(x) for x in zip(true, fitted)]) 
+        
+        fig = plt.figure(figsize=[9,7], dpi=600)
+        ax = fig.add_subplot(111)
+        ax.plot(q, diff)
+        ax.set_xlabel("$\mathregular{Q\ (Å^{-1})}$", fontsize=11, weight='bold')
+        ax.set_ylabel('Difference (arb.)', fontsize=11, weight='bold')
+        plt.show()
 
 def evolution_vs_lbfgs(structure, angle_times, n):
     true = []
@@ -93,9 +144,10 @@ if __name__ == "__main__":
     from structures import thin_layer_sample_1, thin_layer_sample_2
     from structures import easy_sample, many_param_sample
 
-    structure   = similar_sld_sample_2
+    structure   = many_param_sample
     angle_times = {0.7: (70, 5), #Angle: (Points, Time)
                    2.0: (70, 20)}
 
     #fitting_sampling_biases(structure, angle_times, 100)
-    evolution_vs_lbfgs(structure, angle_times, 500)
+    #evolution_vs_lbfgs(structure, angle_times, 500)
+    difference_plots_data(structure, angle_times)
