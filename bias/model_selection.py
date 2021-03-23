270badda0203add219fc8fd5dad8f21e3e048495
James Durant <James.Durant@warwick.ac.uk>
2021-03-23 09:55:59 +0000

    Added KIC

--- bias/model_selection.py
+++ bias/model_selection.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from refnx.dataset import ReflectDataset
 from refnx.reflect import SLD, ReflectModel
 from refnx.analysis import Objective, CurveFitter
 
 from simulate import simulate
+from utils import calc_FIM
 
 class ModelSelection:
-    def __init__(self, layer_bounds=(1,5), sld_bounds=(1,9), thick_bounds=(5,400), rough_bounds=(2,8),
+    def __init__(self, layer_bounds=(1,5), sld_bounds=(1,9), thick_bounds=(5,400), rough_bounds=(1,7),
                  substrate_sld=2.047, default_sld=2, default_thick=200, default_rough=2):
         self.layers_range  = list(range(layer_bounds[0], layer_bounds[1]+1))
         self.sld_bounds    = sld_bounds 
@@ -25,6 +27,7 @@ class ModelSelection:
             layer = SLD(self.default_sld, name="Layer {}".format(i))(thick=self.default_thick, rough=self.default_rough)
             layer.thick.setp(vary=True, bounds=self.thick_bounds)
             layer.sld.real.setp(vary=True, bounds=self.sld_bounds)
+            #layer.rough.setp(vary=True, bounds=self.rough_bounds)
             structure = structure | layer             
             
         substrate = SLD(self.substrate_sld, name="Substrate")(thick=0, rough=self.default_rough)
@@ -33,13 +36,18 @@ class ModelSelection:
         return ReflectModel(structure, scale=1, dq=2, bkg=1e-7)
     
     def select_model(self, data):
-        objectives, logls, AICs, BICs = [], [], [], []
+        q, r, r_error, counts =data[:,0], data[:,1], data[:,2], data[:,3]
+        dataset = ReflectDataset([q, r, r_error])
+        
+        objectives, logls, AICs, BICs, KICs = [], [], [], [], []
         for layers in self.layers_range:
             print(">>> Fitting {}-layer model".format(layers))
             model = self.create_model(layers)
             
-            objective = Objective(model, data)
+            objective = Objective(model, dataset)
             CurveFitter(objective).fit('differential_evolution', verbose=False)
+            
+            xi = objective.varying_parameters()
             
             objectives.append(objective)
             plot_objective(objective)
@@ -47,11 +55,15 @@ class ModelSelection:
             logl = objective.logl()
             logls.append(logl)
 
-            k = len(objective.varying_parameters())
-            AICs.append(2*k - 2*logl)
+            k = len(xi)
+            AICs.append(-2*logl + 2*k)
             
             n = len(data)
-            BICs.append(k*np.log(n) - 2*logl)
+            BICs.append(-2*logl + k*np.log(n))
+            
+            logp = objective.logp()
+            g = calc_FIM([q], xi, counts, [model])
+            KICs.append(-2*logl - 2*logp + k*np.log(n/(2*np.pi)) + np.log(np.linalg.det(g/n)))
             
         print("Log-likelihood: {}-layer".format(np.argmax(logls)+1))  
           
@@ -60,6 +72,9 @@ class ModelSelection:
         
         min_BIC = np.argmin(BICs) 
         print("BIC: {}-layer".format(min_BIC+1))
+        
+        min_KIC = np.argmin(KICs) 
+        print("KIC: {}-layer".format(min_KIC+1))
         
         return objectives[min_AIC]
         
@@ -87,9 +102,9 @@ if __name__ == "__main__":
     from structures import similar_sld_sample_1, similar_sld_sample_2
     from structures import easy_sample, many_param_sample
     
-    angle_times = {0.7: (70, 50), #Angle: (Points, Time)
-                   2.0: (70, 200)}
+    angle_times = {0.7: (70, 5), #Angle: (Points, Time)
+                   2.0: (70, 20)}
 
-    _, data = simulate(easy_sample(), angle_times)
+    _, data = simulate(easy_sample(), angle_times, include_counts=True)
     model = ModelSelection(layer_bounds=(1,4)).select_model(data)
     #print(model)
